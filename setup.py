51c0a53888aa339fa0b386abf53c20cc45c2cf54
Hanzla <hanzlainam204@gmail.com>
2025-10-23 08:05:07 +0500

    Project structure ready,env created

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="src",
+    version="0.0.1",
+    author="Hanzla Inam",
+    author_email="hanzlainam204@gmail.com",
+    packages=find_packages()
+)
